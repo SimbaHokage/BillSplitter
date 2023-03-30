c2e39d0c16d89e662edd8e929feb084c11a834a3
Konrad <konrads0705@wp.pl>
2023-03-30 20:15:33 +0200

    corrected diplays

--- js/script.js
+++ js/script.js
@@ -4,24 +4,26 @@ const selectTip = document.querySelector('#tip')
 const cost = document.querySelector('.cost')
 const error = document.querySelector('.error')
 const countBtn = document.querySelector('.count')
+const costInfo = document.querySelector('.cost-info')
 
 const checkField = () => {
 
     if(inputPrice.value !== '' && inputPeople.value!== '') {
 
         countBill(parseInt(inputPrice.value,10), parseInt(inputPeople.value, 10), parseFloat(selectTip.value, 10))
+        error.textContent = ''
 
     } else {
         error.textContent = 'Uzupełnij wszystkie dane.'
-        cost.textContent = ''
+        costInfo.style.display = 'none'
     }
 
 }
 
 const countBill = (price, peopleNumber, tip) => {
-    console.log(tip);
     let bill = (price + (price * tip)) / peopleNumber
     bill = bill.toFixed(2)
+    costInfo.style.display = 'block'
     cost.textContent = bill
 }
 
